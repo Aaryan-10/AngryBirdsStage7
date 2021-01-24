4a693af780ca7815ddd3653ffb376218ecd3ecf6
Aaryan-10 <aaryan.mishra10052008@gmail.com>
2021-01-24 12:13:48 +0530

    birds trajectory

--- sketch.js
+++ sketch.js
@@ -1,3 +1,82 @@
+/*
+
+//Examples of different types of data in JavaScript
+
+//Number data type
+var num = 45;
+console.log(num);
+
+//String data type
+var name = 'Aaryan'
+console.log(name);
+
+//Boolean(true/false) data type
+var bool = false;
+console.log(bool);
+
+//Undefined data type
+var x;
+console.log(x);
+
+//Reassign the variable x to null
+//Null data type
+x= null;
+console.log(x);
+
+//Array for holding multiple values
+//An array is created inside square brackets 
+
+//Empty array
+var arr1 = [];
+console.log(arr1);
+
+//array with values of same data type
+var arr1 = [12,24,36,48,60];
+console.log(arr1);
+//length of the array -   number of elements or items in the array
+console.log(arr1.length);
+
+//array with values of different data types
+var arr2 = [34.5, 'aaryan', true, null, 'gun', 567];
+console.log(arr2);
+
+//access the values of an array
+console.log(arr1[0]); //12
+console.log(arr1[3]); //48
+console.log(arr2[5]); //567
+console.log(arr2[arr2.length-1]); //567
+console.log(arr1[arr1.length-1]); //60
+
+//looping through the array
+for(var i = 0; i < arr1.length ; i++){
+    console.log(arr1[i]);
+}
+
+//array inside an array
+var arr3 = [34, 56, 'hello', [10,20,30,40,50]];
+console.log(arr3);
+console.log(arr3.length);
+console.log(arr3[2]);//hello
+console.log(arr3[3]);//[10,20,30,40,50]
+console.log(arr3[3][3]); //40
+console.log(arr3[3][0]);//10
+for(var j = 0; j < arr3.length ; j++){
+    console.log(arr3[j]);
+}
+
+//pushing elements in an array
+arr1.push(72);
+console.log(arr1);
+arr1.push(84,96,108);
+console.log(arr1);
+
+//pop the last element of an array
+arr1.pop();
+console.log(arr1);
+
+*/
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,6 +86,8 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+
+var gameState = "onSling";
 
 
 function preload() {
@@ -69,12 +150,16 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState !== 'launched'){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = 'launched';
 }
 
 function keyPressed(){
